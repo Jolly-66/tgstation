66e08f9c89fa9a1ef19b7b6560fe46323b3bc4fc
MrMelbert <kmelbert4@gmail.com>
2021-05-02 02:00:09 -0500

    lints

--- tgui/packages/tgui/interfaces/_LoadoutManager.js
+++ tgui/packages/tgui/interfaces/_LoadoutManager.js
@@ -5,14 +5,11 @@ import { Window } from '../layouts';
 export const _LoadoutManager = (props, context) => {
   const { act, data } = useBackend(context);
   const {
-    icon64,
     selected_loadout,
     loadout_tabs,
     mob_name,
-    ismoth,
     job_clothes,
     tutorial_status,
-    tutorial_text,
   } = data;
 
   const [selectedTabID, setSelectedTab] = useSharedState(
@@ -28,29 +25,7 @@ export const _LoadoutManager = (props, context) => {
       height={650}>
       <Window.Content>
         { !!tutorial_status && (
-          <Dimmer>
-            <Stack
-              vertical
-              align="center">
-              <Stack.Item
-                textAlign="center"
-                fontSize="14px"
-                style={{
-                  'white-space': 'pre-wrap',
-                }}>
-                {tutorial_text}
-              </Stack.Item>
-              <Stack.Item>
-                <Button
-                  mt={1}
-                  align="center"
-                  fontSize="20px"
-                  onClick={() => act('toggle_tutorial')}>
-                    Okay.
-                </Button>
-              </Stack.Item>
-            </Stack>
-          </Dimmer>
+          <LoadoutTutorialDimmer />
         )}
         <Stack grow vertical>
           <Stack.Item>
@@ -62,8 +37,8 @@ export const _LoadoutManager = (props, context) => {
                   icon="info"
                   align="center"
                   content="Tutorial"
-                  onClick={() => act('toggle_tutorial')}/>
-                )}>
+                  onClick={() => act('toggle_tutorial')} />
+              )}>
               <Tabs fluid align="center">
                 {loadout_tabs.map(curTab => (
                   <Tabs.Tab
@@ -79,53 +54,53 @@ export const _LoadoutManager = (props, context) => {
           <Stack.Item>
             <Stack>
               <Stack.Item grow >
-              { selectedTab && selectedTab.contents ? (
-                <Section
-                  title={`Items: ${selectedTab.id}`}
-                  fill
-                  scrollable
-                  buttons={(
-                    <Button.Confirm
-                      icon="times"
-                      color="red"
-                      align="center"
-                      content="Clear All Items"
-                      tooltip="Clears ALL selected items from all categories."
-                      tooltipPosition="bottom-left"
-                      width={10}
-                      onClick={() => act('clear_all_items')}/>
+                { selectedTab && selectedTab.contents ? (
+                  <Section
+                    title={`Items: ${selectedTab.id}`}
+                    fill
+                    scrollable
+                    buttons={(
+                      <Button.Confirm
+                        icon="times"
+                        color="red"
+                        align="center"
+                        content="Clear All Items"
+                        tooltip="Clears ALL selected items from all categories."
+                        tooltipPosition="bottom-left"
+                        width={10}
+                        onClick={() => act('clear_all_items')} />
                     )}>
-                  <Stack vertical>
-                    {selectedTab.contents.map(item => (
-                      <Stack.Item key={item.name}>
-                        <Stack fontSize="15px">
-                          <Stack.Item grow align="left">
-                            {item.name}
-                          </Stack.Item>
-                          <Stack.Item>
-                            <Button.Checkbox
-                              checked={selected_loadout.includes(item.path)}
-                              content="Select"
-                              fluid
-                              onClick={() => act('select_item', {
-                                category: selectedTab.slot,
-                                path: item.path,
-                                doReset: selected_loadout.includes(item.path),
-                              })}/>
-                          </Stack.Item>
-                        </Stack>
-                      </Stack.Item>
-                    ))}
-                  </Stack>
-                </Section>
-              ) : (
-                <Section fill>
-                  <Box>
-                    No contents for selected tab.
-                  </Box>
-                </Section>
-              )}
-            </Stack.Item>
+                    <Stack vertical>
+                      {selectedTab.contents.map(item => (
+                        <Stack.Item key={item.name}>
+                          <Stack fontSize="15px">
+                            <Stack.Item grow align="left">
+                              {item.name}
+                            </Stack.Item>
+                            <Stack.Item>
+                              <Button.Checkbox
+                                checked={selected_loadout.includes(item.path)}
+                                content="Select"
+                                fluid
+                                onClick={() => act('select_item', {
+                                  category: selectedTab.slot,
+                                  path: item.path,
+                                  doReset: selected_loadout.includes(item.path),
+                                })} />
+                            </Stack.Item>
+                          </Stack>
+                        </Stack.Item>
+                      ))}
+                    </Stack>
+                  </Section>
+                ) : (
+                  <Section fill>
+                    <Box>
+                      No contents for selected tab.
+                    </Box>
+                  </Section>
+                )}
+              </Stack.Item>
               <Stack.Item width="50%" align="center">
                 <Section
                   title={`Preview: ${mob_name}`}
@@ -138,68 +113,9 @@ export const _LoadoutManager = (props, context) => {
                         skirt and backpack preference."
                       tooltipPosition="bottom-left"
                       checked={job_clothes}
-                      onClick={() => act('toggle_job_clothes')}/>
-                    )}>
-                  <Stack vertical>
-                    <Stack.Item>
-                      <Box
-                        as="img"
-                        m={0}
-                        src={`data:image/jpeg;base64,${icon64}`}
-                        width="100%"
-                        minWidth={(ismoth ? 48 : 0)}
-                        style={{
-                          '-ms-interpolation-mode': 'nearest-neighbor',
-                        }} />
-                        <Divider/>
-                    </Stack.Item>
-                    <Stack.Item align="center">
-                      <Stack>
-                        <Stack.Item>
-                          <Button
-                            icon="check-double"
-                            color="good"
-                            tooltip="Confirm loadout and exit UI."
-                            tooltipPosition="top"
-                            onClick={() => act('close_ui', {revert: 0,})}/>
-                        </Stack.Item>
-                        <Stack.Item>
-                          <Button
-                            icon="chevron-left"
-                            tooltip="Turn model preview to the left."
-                            tooltipPosition="top"
-                            onClick={() => act('rotate_dummy', {
-                              dir: "left",
-                            })}/>
-                        </Stack.Item>
-                        <Stack.Item>
-                          <Button
-                            icon="sync"
-                            tooltip="Toggle viewing all \
-                              preview directions at once."
-                            tooltipPosition="top"
-                            onClick={() => act('show_all_dirs')}/>
-                        </Stack.Item>
-                        <Stack.Item>
-                          <Button
-                            icon="chevron-right"
-                            tooltip="Turn model preview to the right."
-                            tooltipPosition="top"
-                            onClick={() => act('rotate_dummy', {
-                              dir: "right",
-                            })}/>
-                        </Stack.Item>
-                        <Stack.Item>
-                          <Button
-                            icon="times"
-                            color="bad"
-                            tooltip="Revert loadout and exit UI."
-                            tooltipPosition="top"
-                            onClick={() => act('close_ui', {revert: 1,})}/>
-                        </Stack.Item>
-                      </Stack>
-                    </Stack.Item>
-                  </Stack>
+                      onClick={() => act('toggle_job_clothes')} />
+                  )}>
+                  <LoadoutPreview />
                 </Section>
               </Stack.Item>
             </Stack>
@@ -207,5 +123,107 @@ export const _LoadoutManager = (props, context) => {
         </Stack>
       </Window.Content>
     </Window>
+  );
+};
+
+export const LoadoutTutorialDimmer = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    tutorial_text,
+  } = data;
+  return (
+    <Dimmer>
+      <Stack
+        vertical
+        align="center">
+        <Stack.Item
+          textAlign="center"
+          fontSize="14px"
+          style={{
+            'white-space': 'pre-wrap',
+          }}>
+          {tutorial_text}
+        </Stack.Item>
+        <Stack.Item>
+          <Button
+            mt={1}
+            align="center"
+            fontSize="20px"
+            onClick={() => act('toggle_tutorial')}>
+            Okay.
+          </Button>
+        </Stack.Item>
+      </Stack>
+    </Dimmer>
+  );
+};
+
+export const LoadoutPreview = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    icon64,
+    ismoth,
+  } = data;
+  return (
+    <Stack vertical>
+      <Stack.Item>
+        <Box
+          as="img"
+          m={0}
+          src={`data:image/jpeg;base64,${icon64}`}
+          width="100%"
+          minWidth={(ismoth ? 48 : 0)} // moths are too fat, break the preview
+          style={{
+            '-ms-interpolation-mode': 'nearest-neighbor',
+          }} />
+        <Divider />
+      </Stack.Item>
+      <Stack.Item align="center">
+        <Stack>
+          <Stack.Item>
+            <Button
+              icon="check-double"
+              color="good"
+              tooltip="Confirm loadout and exit UI."
+              tooltipPosition="top"
+              onClick={() => act('close_ui', { revert: 0 })} />
+          </Stack.Item>
+          <Stack.Item>
+            <Button
+              icon="chevron-left"
+              tooltip="Turn model preview to the left."
+              tooltipPosition="top"
+              onClick={() => act('rotate_dummy', {
+                dir: "left",
+              })} />
+          </Stack.Item>
+          <Stack.Item>
+            <Button
+              icon="sync"
+              tooltip="Toggle viewing all \
+              preview directions at once."
+              tooltipPosition="top"
+              onClick={() => act('show_all_dirs')} />
+          </Stack.Item>
+          <Stack.Item>
+            <Button
+              icon="chevron-right"
+              tooltip="Turn model preview to the right."
+              tooltipPosition="top"
+              onClick={() => act('rotate_dummy', {
+                dir: "right",
+              })} />
+          </Stack.Item>
+          <Stack.Item>
+            <Button
+              icon="times"
+              color="bad"
+              tooltip="Revert loadout and exit UI."
+              tooltipPosition="top"
+              onClick={() => act('close_ui', { revert: 1 })} />
+          </Stack.Item>
+        </Stack>
+      </Stack.Item>
+    </Stack>
   );
 };
